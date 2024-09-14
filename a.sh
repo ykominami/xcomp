734361bf5966762ad9f346207add0a632c051c25
ykominami <ykominami@gmail.com>
2024-09-14 22:16:47 +0900

    kekka

---
+++ a.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+yarn add -D react react-dom
+
+yarn add -D typescript @types/react @types/react-dom
+
+yarn add -D babel-loader @babel/runtime @babel/cli @babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react @babel/preset-typescript
+
+yarn add -D rollup rollup-plugin-dts @rollup/plugin-terser rollup-plugin-postcss autoprefixer
+
+yarn add -D @rollup/plugin-commonjs @rollup/plugin-typescript
+
+yarn add -D postcss
+
+yarn add -D tslib
+
