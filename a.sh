b6250d5759f9729841f81213901620a9d46bd11e
ykominami <ykominami@gmail.com>
2024-09-14 23:17:51 +0900

    fix

--- a.sh
+++ a.sh
@@ -5,7 +5,7 @@ yarn add -D typescript @types/react @types/react-dom
 
 yarn add -D babel-loader @babel/runtime @babel/cli @babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react @babel/preset-typescript
 
-yarn add -D rollup @rollup/plugin-dts @rollup/plugin-terser @rollup/plugin-postcss autoprefixer
+yarn add -D rollup rollup-plugin-dts @rollup/plugin-terser rollup-plugin-postcss autoprefixer
 
 yarn add -D @rollup/plugin-commonjs @rollup/plugin-typescript
 
